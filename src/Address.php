1db348d73789b6a33a775b9ffdfdcdb2ebef0fb7
Jason Brown <jasontbrown99@gmail.com>
2017-02-10 08:44:57 -0800

    Address.php created with Contact class with name constructor, includes get, set, and save functions as well as static functions for getall and deleteall

---
+++ src/Address.php
@@ -0,0 +1,37 @@
+<?php
+    class Contact
+        {
+            private $name;
+            }
+
+            function __construct($name)
+            {
+                $this->name = $name;
+            }
+
+            function setName()
+            {
+                $this->name = (string) $new_name;
+            }
+
+            function getName()
+            {
+                return $this->name;
+            }
+
+            function save()
+            {
+                array_push($_SESSION['list_of_contacts'], $this);
+            }
+
+            static function getAll()
+            {
+                return $_SESSION['list_of_contacts'];
+            }
+
+            static function deleteAll()
+            {
+                $_SESSION['list_of_contacts'] = array();
+            }
+        }
+?>
