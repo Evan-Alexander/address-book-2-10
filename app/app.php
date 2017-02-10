c0d0d48f8f2cac9663834fa4a64f68c7701e0a0e
Jason Brown <jasontbrown99@gmail.com>
2017-02-10 14:15:21 -0800

    fix button positioning.

--- app/app.php
+++ app/app.php
@@ -21,12 +21,15 @@
         return $app['twig']->render('contacts.html.twig', array('contacts' => Contact::getAll()));
     });
 
+
     $app->post("/create_contacts", function() use ($app) {
         $contact = new Contact($_POST['name'], $_POST['phone'], $_POST['address']);
         $contact->save();
 
         return $app['twig']->render('create_contacts.html.twig', array('newcontact' => $contact));
     });
+
+    
 
     $app->post("/delete_contacts", function() use ($app) {
         Contact::deleteAll();
